6c57b5a44c6a8805938e5a84860fb21f00383b77
javadebadi <javad.ebadi.1990@gmail.com>
2024-11-11 18:57:04 -0700

    Update export tools

--- crypto_dot_com/data_models/order_history.py
+++ crypto_dot_com/data_models/order_history.py
@@ -1,6 +1,8 @@
 import datetime
+import json
 
 from pydantic import BaseModel
+from pydantic import field_validator
 
 from crypto_dot_com.enums import ExecInstEnum
 from crypto_dot_com.enums import OrderTypeEnum
@@ -37,3 +39,16 @@ class OrderHistoryDataMessage(BaseModel):
     create_time_ns: float
     update_time: int  # ms
     limit_price: float | None = None
+
+    @field_validator("exec_inst", mode="before")
+    def parse_exec_inst(cls, value: str | list[str]) -> list[str]:
+        # Check if the value is a string that looks like a list
+        if isinstance(value, str):
+            try:
+                # Attempt to parse the string as JSON
+                return [ExecInstEnum(value) for value in json.loads(value)]
+            except json.JSONDecodeError:
+                raise ValueError(
+                    "exec_inst must be a valid JSON list of strings"
+                )
+        return value  # If already a list, return as is
