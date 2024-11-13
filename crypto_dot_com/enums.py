7909b6c4cc7c83b694998e3b87d69f3649f81de7
javadebadi <javad.ebadi.1990@gmail.com>
2024-11-12 22:01:12 -0700

    Add time intervale enum and a public api enum

--- crypto_dot_com/enums.py
+++ crypto_dot_com/enums.py
@@ -7,6 +7,7 @@ class CryptoDotComMethodsEnum(StrEnum):
     PRIVATE_CANCEL_ALL_ORDERS = "private/cancel-all-orders"
     PRIVATE_CANCEL_ORDER = "private/cancel-order"
     PRIVATE_GET_ORDER_DETAILS = "private/get-order-detail"
+    PUBLIC_GET_CANDLESTICK = "public/get-candlestick"
 
 
 class SideEnum(StrEnum):
@@ -40,3 +41,18 @@ class StatusEnum(StrEnum):
     FILLED = "FILLED"
     EXPIRED = "EXPIRED"
     ACTIVE = "ACTIVE"
+
+
+class CandlestickTimeInterval(StrEnum):
+    MINUTE_1 = "1m"
+    MINUTE_5 = "5m"
+    MINUTE_15 = "15m"
+    MINUTE_30 = "30m"
+    HOUR_1 = "1h"
+    HOUR_2 = "2h"
+    HOUR_4 = "4h"
+    HOUR_12 = "12h"
+    DAY_1 = "1D"
+    DAY_7 = "7D"
+    DAT_14 = "14D"
+    MONTH_1 = "1M"
